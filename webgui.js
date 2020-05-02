3f79db25f09f36d04e148da9f8e922b8dc096df4
Jered Wierzbicki <jeredw@gmail.com>
2020-05-02 09:25:53 -0700

    move switches to config

--- webgui.js
+++ webgui.js
@@ -1,38 +1,3 @@
-function getTransformToElement(elem, toElement) {
-  return toElement.getScreenCTM().inverse().multiply(elem.getScreenCTM());
-};
-
-function transformedBoundingBox(elem) {
-  // https://stackoverflow.com/questions/10623809/get-bounding-box-of-element-accounting-for-its-transform
-  const svg = elem.ownerSVGElement;
-  const t = getTransformToElement(elem, svg);
-
-  let ps = [svg.createSVGPoint(), svg.createSVGPoint(), svg.createSVGPoint(), svg.createSVGPoint()];
-  let box = elem.getBBox();
-  [ps[0].x, ps[0].y] = [box.x, box.y];
-  [ps[1].x, ps[1].y] = [box.x + box.width, box.y];
-  [ps[2].x, ps[2].y] = [box.x + box.width, box.y + box.height];
-  [ps[3].x, ps[3].y] = [box.x, box.y + box.height];
-
-  let xMin = Infinity;
-  let xMax = -Infinity;
-  let yMin = Infinity;
-  let yMax = -Infinity;
-  for (const p of ps) {
-    const pt = p.matrixTransform(t);
-    xMin = Math.min(xMin, pt.x);
-    xMax = Math.max(xMax, pt.x);
-    yMin = Math.min(yMin, pt.y);
-    yMax = Math.max(yMax, pt.y);
-  }
-
-  box.x = xMin;
-  box.y = yMin;
-  box.width = xMax - xMin;
-  box.height = yMax - yMin;
-  return box;
-}
-
 let source = new EventSource('/events');
 let eniac = null;
 let defaultViewBox = '';
@@ -70,7 +35,7 @@ function addStylesToSvg(doc) {
 
 let adjustRotary = null;
 
-function connectRotarySwitch(selector, settings, onChange=undefined) {
+function connectRotarySwitch(selector, simulatorName, settings, onChange=undefined) {
   const rotary = eniac.querySelector(selector);
   const svg = rotary.ownerSVGElement;
   rotary.style.contain = 'layout paint';
@@ -209,7 +174,7 @@ function connectInitiateElements() {
     }
     rotateNeedle1(Math.random() * 73);
   };
-  connectRotarySwitch('#initiate-dcv1', [
+  connectRotarySwitch('#initiate-dcv1', '', [
     {value: 'A', degrees: -150},
     {value: 'B', degrees: -110},
     {value: 'C', degrees: -80},
@@ -219,7 +184,7 @@ function connectInitiateElements() {
     {value: 'G', degrees: 20},
     {value: 'H', degrees: 54},
   ], showRandomTrace);
-  connectRotarySwitch('#initiate-dcv2', [
+  connectRotarySwitch('#initiate-dcv2', '', [
     {value: '1', degrees: -185},
     {value: '2', degrees: -152},
     {value: '3', degrees: -116},
@@ -233,17 +198,11 @@ function connectInitiateElements() {
     {value: '11', degrees: 117},
   ], showRandomTrace);
   const rotateNeedle2 = makeNeedleRotateable('#initiate-vm2-needle');
-  connectRotarySwitch('#initiate-acv', [
+  connectRotarySwitch('#initiate-acv', '', [
     {value: 'VAB', degrees: -115},
     {value: 'VBC', degrees: -61},
     {value: 'VCA', degrees: 0},
   ], () => rotateNeedle2(Math.random() * 73));
-  connectButton('#initiate-start');
-  connectButton('#initiate-stop');
-  connectButton('#initiate-rs');
-  connectButton('#initiate-clear');
-  connectButton('#initiate-door');
-  connectButton('#initiate-pulse');
 
   makePanelSelectable('#initiate-bottom');
   connectNeon('#initiate-neon-sc1',   (s) => s.initiate[0] == '1');
@@ -272,9 +231,7 @@ function connectCyclingElements() {
   }
   connectNeon('#cycling-neon-10p', () => false);
   connectNeon('#cycling-neon-ccg', () => false);
-
   makePanelSelectable('#cycling-panel');
-  connectToggleSwitch('#cycling-heater-toggle');
   const showPulseTrace = (value) => {
     const traces = eniac.querySelectorAll('.cycling-trace');
     const selected = eniac.querySelector('#cycling-trace-' + value);
@@ -282,7 +239,7 @@ function connectCyclingElements() {
       trace.style.visibility = trace == selected ? '' : 'hidden';
     }
   };
-  connectRotarySwitch('#cycling-osc', [
+  connectRotarySwitch('#cycling-osc', '', [
     {value: 'ext', degrees: -205},
     {value: 'cpp', degrees: -180},
     {value: '10p', degrees: -150},
@@ -296,18 +253,7 @@ function connectCyclingElements() {
     {value: "9p", degrees: 75},
     {value: "scg", degrees: 100},
   ], showPulseTrace);
-  connectRotarySwitch('#cycling-op', [
-    {value: '1p', degrees: -60},
-    {value: '1a', degrees: -30},
-    {value: 'co', degrees: 0},
-  ]);
-  connectButton('#cycling-p');
-
   makePanelSelectable('#cycling-bottom');
-  connectRotarySwitch('#cycling-osc-source', [
-    {value: 'I', degrees: 0},
-    {value: 'E', degrees: 100},
-  ]);
 }
 
 function connectMPElements(panelNumber) {
@@ -331,45 +277,7 @@ function connectMPElements(panelNumber) {
       })(baseStepper + i, value));
     }
   }
-
   makePanelSelectable(`#${prefix}-panel`);
-  connectToggleSwitch(`#${prefix}-heater-toggle`);
-  for (let i = 0; i < steppers.length - 1; i++) {
-    const [s1, s2] = [steppers[i], steppers[i+1]];
-    connectRotarySwitch(`#${prefix}-assoc-${s1}${s2}`, [
-      {value: s1, degrees: -35},
-      {value: s2, degrees: 0},
-    ]);
-  }
-  const decadeSettings = [
-    {value: '0', degrees: -185},
-    {value: '1', degrees: -155},
-    {value: '2', degrees: -115},
-    {value: '3', degrees: -85},
-    {value: '4', degrees: -55},
-    {value: '5', degrees: -25},
-    {value: '6', degrees: 0},
-    {value: '7', degrees: 25},
-    {value: '8', degrees: 55},
-    {value: '9', degrees: 85},
-  ];
-  for (let decade = startDecade; decade > startDecade - 10; decade--) {
-    for (let digit = 1; digit <= 6; digit++) {
-      connectRotarySwitch(`#${prefix}-d${decade}s${digit}`, decadeSettings);
-    }
-  }
-  const clearSettings = [
-    {value: '1', degrees: -110},
-    {value: '2', degrees: -80},
-    {value: '3', degrees: -55},
-    {value: '4', degrees: -30},
-    {value: '5', degrees: 0},
-    {value: '6', degrees: 20},
-  ];
-  for (const s of steppers) {
-    connectRotarySwitch(`#${prefix}-c${s}`, clearSettings);
-  }
-
   makePanelSelectable(`#${prefix}-bottom`);
   for (let i = 0; i < steppers.length; i++) {
     connectNeon(`#${prefix}-neon-${steppers[i]}in`, ((i) => {
@@ -399,43 +307,7 @@ function connectFT1Elements(ftNumber) {
       return (s) => s.ft[ftNumber-1].ring == i;
     })(i));
   }
-
   makePanelSelectable(`${unit} .front-panel`);
-  connectToggleSwitch(`${unit} .heater-toggle`);
-  const opSettings = [
-    {value: 'A-2', degrees: -185},
-    {value: 'A-1', degrees: -150},
-    {value: 'A0', degrees: -120},
-    {value: 'A+1', degrees: -90},
-    {value: 'A+2', degrees: -65},
-    {value: 'S+2', degrees: -30},
-    {value: 'S+1', degrees: 0},
-    {value: 'S0', degrees: 25},
-    {value: 'S-1', degrees: 55},
-    {value: 'S-2', degrees: 85},
-  ];
-  const clearSettings = [
-    {value: '0', degrees: 0},
-    {value: 'NC', degrees: 25},
-    {value: 'C', degrees: 55},
-  ];
-  const repeatSettings = [
-    {value: '1', degrees: -185},
-    {value: '2', degrees: -150},
-    {value: '3', degrees: -115},
-    {value: '4', degrees: -90},
-    {value: '5', degrees: -60},
-    {value: '6', degrees: -25},
-    {value: '7', degrees: 0},
-    {value: '8', degrees: 30},
-    {value: '9', degrees: 60},
-  ];
-  for (let i = 1; i <= 11; i++) {
-    connectRotarySwitch(`${unit} .op${i}`, opSettings);
-    connectRotarySwitch(`${unit} .cl${i}`, clearSettings);
-    connectRotarySwitch(`${unit} .rp${i}`, repeatSettings);
-  }
-
   makePanelSelectable(`${unit} .bottom-panel`);
   for (let p = 0; p <= 10; p++) {
     connectNeon(`${unit} .inff${p}`, ((p) => {
@@ -447,46 +319,6 @@ function connectFT1Elements(ftNumber) {
 function connectFT2Elements(ftNumber) {
   const unit = `#ft${ftNumber}-2`;
   makePanelSelectable(`${unit} .front-panel`);
-  connectToggleSwitch(`${unit} .heater-toggle`);
-  const pmSettings = [
-    {value: 'P', degrees: -60},
-    {value: 'M', degrees: -30},
-    {value: 'T', degrees: 0},
-  ];
-  connectRotarySwitch(`${unit} .mpm1`, pmSettings);
-  connectRotarySwitch(`${unit} .mpm2`, pmSettings);
-  const deleteSettings = [
-    {value: 'D', degrees: 0},
-    {value: 'O', degrees: 50},
-  ];
-  const constantSettings = [
-    {value: '0', degrees: -215},
-    {value: '1', degrees: -180},
-    {value: '2', degrees: -150},
-    {value: '3', degrees: -120},
-    {value: '4', degrees: -95},
-    {value: '5', degrees: -60},
-    {value: '6', degrees: -30},
-    {value: '7', degrees: 0},
-    {value: '8', degrees: 20},
-    {value: '9', degrees: 55},
-    {value: 'PM1', degrees: 80},
-    {value: 'PM2', degrees: 110},
-  ];
-  for (let i = 1; i <= 4; i++) {
-    connectRotarySwitch(`${unit} .a${i}d`, deleteSettings);
-    connectRotarySwitch(`${unit} .b${i}d`, deleteSettings);
-    connectRotarySwitch(`${unit} .a${i}c`, constantSettings);
-    connectRotarySwitch(`${unit} .b${i}c`, constantSettings);
-  }
-  const subtractSettings = [
-    {value: '0', degrees: -50},
-    {value: 'S', degrees: 0},
-  ];
-  for (let i = 5; i <= 10; i++) {
-    connectRotarySwitch(`${unit} .a${i}s`, subtractSettings);
-    connectRotarySwitch(`${unit} .b${i}s`, subtractSettings);
-  }
 }
 
 function connectAccumulatorElements(accNumber) {
@@ -504,57 +336,7 @@ function connectAccumulatorElements(accNumber) {
       })(decade, value));
     }
   }
-
   makePanelSelectable(`${unit} .front-panel`);
-  connectToggleSwitch(`${unit} .heater-toggle`);
-  connectRotarySwitch(`${unit} .sf`, [
-    {value: '10', degrees: -185},
-    {value: '9', degrees: -150},
-    {value: '8', degrees: -125},
-    {value: '7', degrees: -95},
-    {value: '6', degrees: -65},
-    {value: '5', degrees: -35},
-    {value: '4', degrees: 0},
-    {value: '3', degrees: 30},
-    {value: '2', degrees: 60},
-    {value: '1', degrees: 90},
-    {value: '0', degrees: 120},
-  ]);
-  connectRotarySwitch(`${unit} .sc`, [
-    {value: 'C', degrees: 0},
-    {value: '0', degrees: -50},
-  ]);
-  for (let i = 1; i <= 12; i++) {
-    connectRotarySwitch(`${unit} .op${i}`, [
-      {value: 'α', degrees: -150},
-      {value: 'β', degrees: -125},
-      {value: 'γ', degrees: -90},
-      {value: 'δ', degrees: -65},
-      {value: 'ε', degrees: -32},
-      {value: '0', degrees: 0},
-      {value: 'A', degrees: 30},
-      {value: 'AS', degrees: 55},
-      {value: 'S', degrees: 85},
-    ]);
-    connectRotarySwitch(`${unit} .cc${i}`, [
-      {value: 'C', degrees: 0},
-      {value: '0', degrees: -75},
-    ]);
-  }
-  for (let i = 5; i <= 12; i++) {
-    connectRotarySwitch(`${unit} .rp${i}`, [
-      {value: '1', degrees: -185},
-      {value: '2', degrees: -150},
-      {value: '3', degrees: -115},
-      {value: '4', degrees: -90},
-      {value: '5', degrees: -60},
-      {value: '6', degrees: -25},
-      {value: '7', degrees: 0},
-      {value: '8', degrees: 30},
-      {value: '9', degrees: 60},
-    ]);
-  }
-
   makePanelSelectable(`${unit} .bottom-panel`);
   for (let i = 1; i <= 12; i++) {
     connectNeon(`${unit} .p${i}`, ((i) => {
@@ -605,57 +387,7 @@ function connectDividerElements() {
       return (s) => s.div.placeRing == i - 1;
     })(i));
   }
-
   makePanelSelectable('#div-front-panel');
-  connectToggleSwitch('#div-heater-toggle');
-  for (let i = 1; i <= 8; i++) {
-    connectRotarySwitch(`#div-nr${i}`, [
-      {value: 'α', degrees: -120},
-      {value: 'β', degrees: -60},
-      {value: '0', degrees: 0},
-    ]);
-    connectRotarySwitch(`#div-nc${i}`, [
-      {value: 'C', degrees: 45},
-      {value: '0', degrees: 0},
-    ]);
-    connectRotarySwitch(`#div-dr${i}`, [
-      {value: 'α', degrees: -120},
-      {value: 'β', degrees: -60},
-      {value: '0', degrees: 0},
-    ]);
-    connectRotarySwitch(`#div-dc${i}`, [
-      {value: 'C', degrees: 45},
-      {value: '0', degrees: 0},
-    ]);
-    connectRotarySwitch(`#div-pl${i}`, [
-      {value: 'D4', degrees: -180},
-      {value: 'D7', degrees: -150},
-      {value: 'D8', degrees: -115},
-      {value: 'D9', degrees: -90},
-      {value: 'D10', degrees: -55},
-      {value: 'S4', degrees: -20},
-      {value: 'S7', degrees: 0},
-      {value: 'S8', degrees: 35},
-      {value: 'S9', degrees: 65},
-      {value: 'S10', degrees: 90},
-    ]);
-    connectRotarySwitch(`#div-ro${i}`, [
-      {value: 'RO', degrees: 0},
-      {value: 'NRO', degrees: -45},
-    ]);
-    connectRotarySwitch(`#div-an${i}`, [
-      {value: '1', degrees: -130},
-      {value: '2', degrees: -100},
-      {value: '3', degrees: -70},
-      {value: '4', degrees: -40},
-      {value: 'OFF', degrees: 0},
-    ]);
-    connectRotarySwitch(`#div-il${i}`, [
-      {value: 'I', degrees: 0},
-      {value: 'NI', degrees: -30},
-    ]);
-  }
-
   makePanelSelectable('#div-bottom');
   for (let i = 1; i <= 8; i++) {
     connectNeon(`#div-prog${i}`, ((i) => {
@@ -683,57 +415,8 @@ function connectMultiplierElements(panelNumber) {
     connectNeon('#mult3-reset', (s) => s.mult.reset3);
   }
   makePanelSelectable(`#${prefix}-front-panel`);
-  connectToggleSwitch(`#${prefix}-heater-toggle`);
   const startDigit = [1, 9, 17][panelNumber - 1];
   for (let i = startDigit; i < startDigit + 8; i++) {
-    const ierIcandSettings = [
-      {value: 'α', degrees: -120},
-      {value: 'β', degrees: -90},
-      {value: 'γ', degrees: -60},
-      {value: 'δ', degrees: -30},
-      {value: 'ε', degrees: 0},
-      {value: '0', degrees: 25},
-    ];
-    const ierIcandClearSettings = [
-      {value: 'C', degrees: 45},
-      {value: '0', degrees: 0},
-    ];
-    connectRotarySwitch(`#${prefix}-ieracc${i}`, ierIcandSettings);
-    connectRotarySwitch(`#${prefix}-iercl${i}`, ierIcandClearSettings);
-    connectRotarySwitch(`#${prefix}-icandacc${i}`, ierIcandSettings);
-    connectRotarySwitch(`#${prefix}-icandcl${i}`, ierIcandClearSettings);
-    connectRotarySwitch(`#${prefix}-sf${i}`, [
-      {value: '10', degrees: -190},
-      {value: '9', degrees: -155},
-      {value: '8', degrees: -125},
-      {value: '7', degrees: -95},
-      {value: '6', degrees: -65},
-      {value: '5', degrees: -35},
-      {value: '4', degrees: 0},
-      {value: '3', degrees: 25},
-      {value: '2', degrees: 55},
-      {value: '0', degrees: 85},
-    ]);
-    connectRotarySwitch(`#${prefix}-place${i}`, [
-      {value: '2', degrees: -180},
-      {value: '3', degrees: -150},
-      {value: '4', degrees: -120},
-      {value: '5', degrees: -90},
-      {value: '6', degrees: -55},
-      {value: '7', degrees: -25},
-      {value: '8', degrees: 0},
-      {value: '9', degrees: 30},
-      {value: '10', degrees: 60},
-    ]);
-    connectRotarySwitch(`#${prefix}-prod${i}`, [
-      {value: 'A', degrees: -160},
-      {value: 'S', degrees: -130},
-      {value: 'AS', degrees: -90},
-      {value: '0', degrees: -60},
-      {value: 'AC', degrees: -25},
-      {value: 'SC', degrees: 0},
-      {value: 'ASC', degrees: 30},
-    ]);
     connectNeon(`#${prefix}-prog${i}`, ((i) => {
       return (s) => s.mult.program[i-1];
     })(i));
@@ -749,81 +432,21 @@ function connectCT1Elements() {
       return (s) => s.constant[i-1] == '1';
     })(i));
   }
-  
-  connectToggleSwitch('#ct1-heater-toggle');
-  const constantNames = 'abcdefghjk';
-  for (let i = 1; i <= 30; i++) {
-    const k = ~~((i - 1) / 6) * 2;
-    const [c1, c2] = [constantNames[k], constantNames[k+1]];
-    connectRotarySwitch(`#ct1-s${i}`, [
-      {value: `${c1}l`, degrees: -120},
-      {value: `${c1}r`, degrees: -90},
-      {value: `${c1}lr`, degrees: -60},
-      {value: `${c2}l`, degrees: -20},
-      {value: `${c2}r`, degrees: 0},
-      {value: `${c2}lr`, degrees: 25},
-    ]);
-  }
 }
 
-function connectCT2Elements() {
-  makePanelSelectable('#ct2-front-panel');
-  connectToggleSwitch('#ct2-heater-toggle');
-  const pmSettings = [
-    {value: 'P', degrees: 0},
-    {value: 'M', degrees: -60},
-  ];
-  connectRotarySwitch('#ct2-jl', pmSettings);
-  connectRotarySwitch('#ct2-jr', pmSettings);
-  connectRotarySwitch('#ct2-kl', pmSettings);
-  connectRotarySwitch('#ct2-kr', pmSettings);
-  const digitSettings = [
-    {value: '0', degrees: -175},
-    {value: '1', degrees: -150},
-    {value: '2', degrees: -120},
-    {value: '3', degrees: -90},
-    {value: '4', degrees: -60},
-    {value: '5', degrees: -30},
-    {value: '6', degrees: 0},
-    {value: '7', degrees: 25},
-    {value: '8', degrees: 55},
-    {value: '9', degrees: 85},
-  ];
-  for (let i = 1; i <= 10; i++) {
-    connectRotarySwitch(`#ct2-j${i}`, digitSettings);
-    connectRotarySwitch(`#ct2-k${i}`, digitSettings);
-  }
-}
-
-function connectPR1Elements() {
-  makePanelSelectable('#pr1-front-panel');
-  for (let i = 1; i <= 8; i++) {
-    connectRotarySwitch(`#pr1-${i}-${i+1}`, [
-      {value: '0', degrees: -60},
-      {value: 'C', degrees: 0},
-    ]);
-  }
-}
-
-function connectPR2Elements() {
-  makePanelSelectable('#pr2-front-panel');
-  connectToggleSwitch('#pr2-heater-toggle');
-  for (let i = 1; i <= 16; i++) {
-    connectRotarySwitch(`#pr2-${i}`, [
-      {value: '0', degrees: -60},
-      {value: 'p', degrees: 0},
-    ]);
-  }
-}
-
-function connectPR3Elements() {
-  makePanelSelectable('#pr3-front-panel');
-  for (let i = 9; i <= 16; i++) {
-    const n = i == 16 ? 1 : i + 1;
-    connectRotarySwitch(`#pr3-${i}-${n}`, [
-      {value: '0', degrees: -60},
-      {value: 'C', degrees: 0},
-    ]);
+function connectSwitches(config) {
+  for (const [selector, s] of Object.entries(config)) {
+    switch (s.type) {
+    case 'rotary':
+      connectRotarySwitch(selector, s.simulatorName, s.settings);
+      break;
+    case 'toggle':
+      connectToggleSwitch(selector);
+      break;
+    case 'button':
+      connectButton(selector);
+      break;
+    }
   }
 }
 
@@ -833,6 +456,16 @@ window.onload = (event) => {
   addStylesToSvg(wrapperDoc);
   eniac = wrapperDoc.querySelector('#eniac');
   defaultViewBox = eniac.getAttribute('viewBox');
+
+  fetch('/switches.json').then((response) => {
+    if (response.status != 200) {
+      console.error('switches.json: ', response.status);
+      return;
+    }
+    response.json().then((config) => connectSwitches(config))
+      .catch((e) => console.error(e));
+  }).catch((e) => console.error(e));
+
   connectInitiateElements();
   connectCyclingElements();
   connectMPElements(1);
@@ -849,11 +482,11 @@ window.onload = (event) => {
   connectMultiplierElements(2);
   connectMultiplierElements(3);
   connectCT1Elements();
-  connectCT2Elements();
+  makePanelSelectable('#ct2-front-panel');
   makePanelSelectable('#ct3-front-panel');
-  connectPR1Elements();
-  connectPR2Elements();
-  connectPR3Elements();
+  makePanelSelectable('#pr1-front-panel');
+  makePanelSelectable('#pr2-front-panel');
+  makePanelSelectable('#pr3-front-panel');
 
   requestAnimationFrame(step);
 
@@ -876,4 +509,39 @@ function viewElementSelector(name) {
 
 function viewDefault() {
   eniac.setAttribute('viewBox', defaultViewBox);
+}
+
+function getTransformToElement(elem, toElement) {
+  return toElement.getScreenCTM().inverse().multiply(elem.getScreenCTM());
+}
+
+function transformedBoundingBox(elem) {
+  // https://stackoverflow.com/questions/10623809/get-bounding-box-of-element-accounting-for-its-transform
+  const svg = elem.ownerSVGElement;
+  const t = getTransformToElement(elem, svg);
+
+  let ps = [svg.createSVGPoint(), svg.createSVGPoint(), svg.createSVGPoint(), svg.createSVGPoint()];
+  let box = elem.getBBox();
+  [ps[0].x, ps[0].y] = [box.x, box.y];
+  [ps[1].x, ps[1].y] = [box.x + box.width, box.y];
+  [ps[2].x, ps[2].y] = [box.x + box.width, box.y + box.height];
+  [ps[3].x, ps[3].y] = [box.x, box.y + box.height];
+
+  let xMin = Infinity;
+  let xMax = -Infinity;
+  let yMin = Infinity;
+  let yMax = -Infinity;
+  for (const p of ps) {
+    const pt = p.matrixTransform(t);
+    xMin = Math.min(xMin, pt.x);
+    xMax = Math.max(xMax, pt.x);
+    yMin = Math.min(yMin, pt.y);
+    yMax = Math.max(yMax, pt.y);
+  }
+
+  box.x = xMin;
+  box.y = yMin;
+  box.width = xMax - xMin;
+  box.height = yMax - yMin;
+  return box;
 }
