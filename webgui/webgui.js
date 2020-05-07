f7c1035561dc13fb313c516cac35567fb6cf00ad
Jered Wierzbicki <jeredw@gmail.com>
2020-05-07 08:51:20 -0700

    add trays back and connect ports

--- webgui/webgui.js
+++ webgui/webgui.js
@@ -360,6 +360,147 @@ function connectController() {
   connectButton(doc.querySelector('#single-step'), 'p');
 }
 
+let ports = {};
+function connectPort(selector, simulatorName) {
+  const element = eniac.querySelector(selector);
+  if (!element) {
+    console.error("element not found", selector);
+  }
+  ports[selector] = {
+    simulatorName: simulatorName
+  };
+}
+
+function connectPorts() {
+  for (let i = 1; i <= 6; i++) {
+    connectPort(`#initiate .p-ci${i}`, `i.ci${i}`);
+    connectPort(`#initiate .p-co${i}`, `i.co${i}`);
+  }
+  connectPort('#initiate .p-ri', 'i.ri');
+  connectPort('#initiate .p-ro', 'i.ro');
+  connectPort('#initiate .p-rl', 'i.rl');
+  connectPort('#initiate .p-pi', 'i.pi');
+  connectPort('#initiate .p-po', 'i.po');
+  connectPort('#initiate .p-io', 'i.io');
+
+  connectPort('#ct-1 .p-o', 'c.o');
+  for (let i = 1; i <= 30; i++) {
+    connectPort(`#ct-1 .p-${i}i`, `c.${i}i`);
+    connectPort(`#ct-1 .p-${i}o`, `c.${i}o`);
+  }
+
+  for (let i = 1; i <= 20; i++) {
+    connectPort(`#accumulator-${i} .alpha`, `a${i}.α`);
+    connectPort(`#accumulator-${i} .beta`, `a${i}.β`);
+    connectPort(`#accumulator-${i} .gamma`, `a${i}.γ`);
+    connectPort(`#accumulator-${i} .delta`, `a${i}.δ`);
+    connectPort(`#accumulator-${i} .epsilon`, `a${i}.ε`);
+    connectPort(`#accumulator-${i} .output-a`, `a${i}.A`);
+    connectPort(`#accumulator-${i} .output-s`, `a${i}.S`);
+    for (let j = 1; j <= 12; j++) {
+      connectPort(`#accumulator-${i} .p-${j}i`, `a${i}.${j}i`);
+      if (j > 4) {
+        connectPort(`#accumulator-${i} .p-${j}o`, `a${i}.${j}o`);
+      }
+    }
+  }
+
+  connectPort('#multiplier-1 .p-ralpha', 'm.rα');
+  connectPort('#multiplier-1 .p-rbeta', 'm.rβ');
+  connectPort('#multiplier-1 .p-rgamma', 'm.rγ');
+  connectPort('#multiplier-1 .p-rdelta', 'm.rδ');
+  connectPort('#multiplier-1 .p-repsilon', 'm.rε');
+  connectPort('#multiplier-1 .p-dalpha', 'm.dα');
+  connectPort('#multiplier-1 .p-dbeta', 'm.dβ');
+  connectPort('#multiplier-1 .p-dgamma', 'm.dγ');
+  connectPort('#multiplier-1 .p-ddelta', 'm.dδ');
+  connectPort('#multiplier-1 .p-depsilon', 'm.dε');
+  connectPort('#multiplier-3 .p-lhppi', 'm.lhppi');
+  connectPort('#multiplier-3 .p-lhppii', 'm.lhppii');
+  connectPort('#multiplier-3 .p-rhppi', 'm.rhppi');
+  connectPort('#multiplier-3 .p-rhppii', 'm.rhppii');
+  connectPort('#multiplier-3 .p-a', 'm.a');
+  connectPort('#multiplier-3 .p-s', 'm.s');
+  connectPort('#multiplier-3 .p-as', 'm.as');
+  connectPort('#multiplier-3 .p-ac', 'm.ac');
+  connectPort('#multiplier-3 .p-sc', 'm.sc');
+  connectPort('#multiplier-3 .p-asc', 'm.asc');
+  connectPort('#multiplier-3 .p-rs', 'm.rs');
+  connectPort('#multiplier-3 .p-ds', 'm.ds');
+  connectPort('#multiplier-3 .p-f', 'm.f');
+  for (let i = 1; i <= 24; i++) {
+    const m = i < 9 ? 1 : i < 17 ? 2 : 3;
+    connectPort(`#multiplier-${m} .p-${i}i`, `m.${i}i`);
+    connectPort(`#multiplier-${m} .p-${i}o`, `m.${i}o`);
+  }
+
+  connectPort(`#divider .p-ans`, 'd.ans');
+  for (let i = 1; i <= 8; i++) {
+    connectPort(`#divider .p-${i}i`, `d.${i}i`);
+    connectPort(`#divider .p-${i}o`, `d.${i}o`);
+    connectPort(`#divider .p-${i}l`, `d.${i}l`);
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    connectPort(`#ft${i}-1 .j-nc`, `f${i}.NC`);
+    connectPort(`#ft${i}-1 .j-c`, `f${i}.C`);
+    connectPort(`#ft${i}-1 .j-arg`, `f${i}.arg`);
+    for (let j = 1; j <= 11; j++) {
+      connectPort(`#ft${i}-1 .j-${j}i`, `f${i}.${j}i`);
+      connectPort(`#ft${i}-1 .j-${j}o`, `f${i}.${j}o`);
+    }
+    connectPort(`#ft${i}-2 .p-a`, `f${i}.A`);
+    connectPort(`#ft${i}-2 .p-b`, `f${i}.B`);
+  }
+
+  for (let i = 1; i <= 20; i++) {
+    const m = i <= 10 ? 2 : 1;
+    connectPort(`#mp-${m} .p-${i}di`, `p.${i}di`);
+  }
+  for (let i = 0; i < 10; i++) {
+    const m = i < 5 ? 1 : 2;
+    const s = "ABCDEFGHJK"[i];
+    connectPort(`#mp-${m} .p-${s}di`.toLowerCase(), `p.${s}di`);
+    connectPort(`#mp-${m} .p-${s}i`.toLowerCase(), `p.${s}i`);
+    connectPort(`#mp-${m} .p-${s}cdi`.toLowerCase(), `p.${s}cdi`);
+    for (let j = 1; j <= 6; j++) {
+      connectPort(`#mp-${m} .p-${s}${j}o`.toLowerCase(), `p.${s}${j}o`);
+    }
+  }
+
+  for (let i = 1; i <= 9; i++) {
+    connectPort(`.dt-${i}`, `${i}`);
+  }
+  for (let i = 1; i <= 11; i++) {
+    for (let j = 1; j <= 11; j++) {
+      connectPort(`.ct-${i}-${j}`, `${i}-${j}`);
+    }
+  }
+}
+
+async function setupWiringFromSimulator() {
+  let portNames = [];
+  let selectors = [];
+  let commands = [];
+  for (const selector of Object.keys(ports)) {
+    const port = ports[selector].simulatorName;
+    portNames.push(port);
+    selectors.push(selector);
+    commands.push(`p? ${port}`);
+  }
+  let outputs = await runCommands(commands);
+  for (let i = 0; i < outputs.length; i++) {
+    const port = portNames[i];
+    const selector = selectors[i];
+    const value = outputs[i].trim();
+    const elem = eniac.querySelector(selector);
+    if (value === 'unconnected') {
+      //console.log(port, value);
+      elem.style.opacity = '0.5';
+    }
+  }
+}
+
 window.onload = (event) => {
   const wrapper = document.querySelector('#eniac');
   const wrapperDoc = wrapper.contentDocument;
@@ -373,6 +514,8 @@ window.onload = (event) => {
     .then(setSwitchesToSimulatorValues);
   fetchConfig('neons.json').then(configureNeons);
   fetchConfig('panels.json').then(configurePanels);
+  connectPorts();
+  setupWiringFromSimulator();
   connectInitiateElements();
   connectCyclingElements();
 
@@ -389,13 +532,19 @@ window.onload = (event) => {
   });
 }
 
+function setTrayVisibility(visibility) {
+  const dtrays = eniac.querySelector('.dtrays');
+  dtrays.style.visibility = visibility;
+  const ctrays = eniac.querySelector('.ctrays');
+  ctrays.style.visibility = visibility;
+}
+
 let oldScroll = 0;
 function viewElementSelector(name) {
   let elem = eniac.querySelector(name);
   let box = transformedBoundingBox(elem);
   eniac.setAttribute('viewBox', `${box.x} ${box.y} ${box.width} ${box.height}`);
-  const trays = document.querySelector('.trays');
-  trays.style.visibility = 'hidden';
+  setTrayVisibility('hidden');  // helps animation performance
   oldScroll = document.scrollingElement.scrollLeft;
   document.scrollingElement.scrollLeft = 0;
   document.querySelector('.vis').style.overflow = 'hidden';
@@ -403,8 +552,7 @@ function viewElementSelector(name) {
 
 function viewDefault() {
   eniac.setAttribute('viewBox', defaultViewBox);
-  const trays = document.querySelector('.trays');
-  trays.style.visibility = '';
+  setTrayVisibility('');
   document.querySelector('.vis').style.overflow = '';
   document.scrollingElement.scrollLeft = oldScroll;
 }
